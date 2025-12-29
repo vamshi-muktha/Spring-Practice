2819bf03ba7ceb63eae7167ae50770e3beed53aa
labuser <labuser@ip-172-31-4-121.ap-south-1.compute.internal>
2025-12-29 14:56:10 +0000

    Creds save

--- BookEx/src/main/java/com/vamshi/bookex/BookExApplication.java
+++ BookEx/src/main/java/com/vamshi/bookex/BookExApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookExApplication {   
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookExApplication.class, args);
+		SpringApplication.run(BookExApplication.class, args);   
 	}
 
 }
