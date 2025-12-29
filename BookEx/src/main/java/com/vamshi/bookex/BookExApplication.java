adf1944fb4e5018d87b8bfa8bd59d765f2074a79
labuser <labuser@ip-172-31-4-121.ap-south-1.compute.internal>
2025-12-29 14:54:51 +0000

    minor fix

--- BookEx/src/main/java/com/vamshi/bookex/BookExApplication.java
+++ BookEx/src/main/java/com/vamshi/bookex/BookExApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookExApplication {
+public class BookExApplication {   
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookExApplication.class, args);
